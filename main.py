fecbf64290a11b7cec96e87f22c3515d287b6e6a
ReturnOfTheLast <returnofthelast@gmail.com>
2022-09-15 08:32:50 +0200

    Commented and cleaned up

--- main.py
+++ main.py
@@ -1,47 +1,43 @@
-field = [i for i in range(1, 10)]
-players = ['O', 'X']
+board = [i for i in range(1, 10)] # Make a board as a list with the numbers 1-9
+players = ['X', 'O'] # Players and their symbol
 
-def print_field() -> None:
-    for i in range(3):
-        print_horizontal_line()
-        for c in field[i*3:i*3+3]: print(f"| {c} ", end="")
-        print("|")
-    print_horizontal_line()
+def print_board() -> None: # Method to print the board
+    for i in range(3): # Repeat three times one for each row
+        print_horizontal_line() # Print the line at the top and in between the rows
+        for c in board[i*3:i*3+3]: print(f"| {c} ", end="") # Print the value for each of the three columns in the row (with a divider to the left)
+        print("|") # Print the final divider (rightmost) at the end of the row
+    print_horizontal_line() # Print the bottom line
 
-def print_horizontal_line() -> None: print("-" * 13)
+def print_horizontal_line() -> None: print("-" * 13) # Method to print the horizontal lines
 
-def ask_for_move(player: int) -> None:
-    global field
-    while True:
-        try: answer = int(input(f"Spiller {players[player]} hvad er dit træk? "))
-        except ValueError: print("Input skal være et tal"); continue
-        if 9 >= answer >= 1 and isinstance(field[answer-1], int):
-            field[answer-1] = players[player]
-            break
-        else: print("Det er ikke et lovligt træk")
+def ask_for_move(player: int) -> None: # Method to get user input
+    global board # Make the board list editable in the method
+    while True: # Try to get valid input till we succeed
+        try: answer = int(input(f"Player {players[player]} what's your move? ")) # Get user input and try to convert it to an integer
+        except ValueError: print("Input must be a number"); continue # If the input cannot be converted we show an error and continue at the start of the loop
+        if 9 >= answer >= 1 and isinstance(board[answer-1], int): board[answer-1] = players[player]; break # If the user input is 1-9 and the board position haven't been taken yet we change the board position to the players symbol and break the loop
+        else: print("Illegal move") # If not, show error and repeat the loop
 
-def print_winner(winner: str):
-    print_field()
-    print(f"Spiller {winner} har vundet")
+def print_winner(winner: str) -> None: print_board(); print(f"Player {winner} has won!") # Method to print the winning player
 
-def check_if_won() -> bool:
-    for i in range(3):
-        if field[i] == field[i+3] == field[i+6]:
-            print_winner(field[i])
+def check_if_won() -> bool: # Method to check if a player has won
+    for i in range(3): # Check rows and columns at the same time (three rows and three columns)
+        if board[i] == board[i+3] == board[i+6]: # Check if the columns have the same symbol
+            print_winner(board[i]) # Print the winning player/symbol
             return True
-        elif field[i*3] == field[i*3+1] == field[i*3+2]:
-            print_winner(field[i*3])
+        elif board[i*3] == board[i*3+1] == board[i*3+2]: # Check if the rows have the same symbol
+            print_winner(board[i*3]) # Print the winning player/symbol
             return True
-    if field[0] == field[4] == field[8] or field[2] == field[4] == field[6]:
-        print_winner(field[4])
+    if board[0] == board[4] == board[8] or board[2] == board[4] == board[6]: # Check the diagonals
+        print_winner(board[4]) # Print the winning player/symbols
         return True
     return False
 
-def main():
-    for i in range(9):
-        print_field()
-        ask_for_move(i%2)
-        if check_if_won(): exit(1)
-    print("Uafgjort")
+def main(): # Main method
+    for i in range(9): # Play for nine moves (number of total position in the board)
+        print_board() # Print the board
+        ask_for_move(i%2) # Ask for the players move (alternates as the 'i' goes from 0 to 8 since the modulo 2 will make it swing back and forth between 0 and 1)
+        if check_if_won(): exit(1) # Exit the program if a player has won
+    print("Game ends in a tie") # If all 9 moves go by and the game haven't been won, it will end in a tie
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": main() # Check that the file is run as the primary program and not imported
